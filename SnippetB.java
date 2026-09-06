512cef286f58c1eff93700655d32410e1063c8a0
Dissan Tech <stuart1230x@gmail.com>
2026-09-06 12:31:59 +0300

    Fix bug by initializing bonusMarks before use

--- SnippetB.java
+++ SnippetB.java
@@ -1,3 +1,10 @@
+//The bug here is that we are trying to add an int variable (bonusMarks) to a double variable (examMark) without initializing bonusMarks.
 public class SnippetB {
+    public static void main(String[] args){
+        int bonusMarks = 20; 
+        double examMark = 68.0; 
+        double finalMark = examMark + bonusMarks; 
+        System.out.println("Final mark: " + finalMark); 
+    }
     
 }
